102c3265319353474ba1122bf08b067b1f3d154c
Giulia Gallo <giulia.gallo2903@gmail.com>
2025-01-15 15:42:30 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,53 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+class Product {
+    private String nome;
+    private String category;
+    private double price;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public Product(String nome, String category, double price) {
+        this.nome = nome;
+        this.category = category;
+        this.price = price;
     }
+
+    public String getNome() {
+            return nome;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public String getCategory() {
+        return category;
+    }
+    @Override
+    public String toString() {
+        return "Product{nome='" + nome + "', category='" + category + "', price=" + price + "}";
+    }
+   public class StreamExample {
+        public static void main(String[] args) {
+            List<Product> products = Arrays.asList(
+                    new Product("A", "Book", 120),
+                    new Product("B", "Book", 20),
+                    new Product("C", "Book", 80)
+            );
+
+            List<Product> filteredProduct =products.stream()
+                    .filter(product -> "Books".equalsIgnoreCase(product.getCategory()))
+                    .filter(p -> p.getPrice() > 100) // Filtra per prezzo > 100
+                    .collect(Collectors.toList());
+            System.out.println(filteredProduct);
+
+
+
+        }
+        
+   }
 }
